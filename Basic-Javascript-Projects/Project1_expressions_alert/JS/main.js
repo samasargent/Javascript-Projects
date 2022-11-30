5dba214b0a08911608950952ba207465e1aad972
Samantha Sargent <samjsargent07@gmail.com>
2022-11-30 15:01:58 -0800

    Added JS to project 1

---
+++ Basic-Javascript-Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,8 @@
+window.alert(4 + 4); //Alert window with value of 4 + 4
+var Family = "The Sargents", Dad = "Duncan", Mom = "Celeste",
+    Daughter = "Laurel", Son = "Peter"; //Defining mutiple variables 
+document.write(Dad); //Printing variable string "Dad"
+document.write("<br><br>"); //Printing line breaks to give space
+var Sent1 = "This is the beginning of the string", 
+    Sent2 = " and this is the end of the string"; //Defining Sent1 and Sent2
+document.write(Sent1 + Sent2); //Printing concatenated strings Sent1 and Sent2
